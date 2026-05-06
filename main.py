0162a54e8c49648687738270e12c66242a5e18b9
bimaryan <bimaryan046@gmail.com>
2026-05-06 15:01:38 +0700

    chore: update uvicorn server port from 8000 to 8111

--- main.py
+++ main.py
@@ -186,4 +186,4 @@ def proses_klasifikasi(request: ChatRequest):
 # ==========================================
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8111, reload=True)
